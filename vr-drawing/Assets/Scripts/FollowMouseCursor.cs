300a176a1b316d8257eb0d34fa34b2dffc363416
Seongju <seongju0007@gmail.com>
2022-07-30 16:54:37 +0900

    Update

--- vr-drawing/Assets/Scripts/FollowMouseCursor.cs
+++ vr-drawing/Assets/Scripts/FollowMouseCursor.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class FollowMouseCursor : MonoBehaviour
 {
+    // 문자에 태그를 걸고
+    // 클릭 시 마우스 포인터(스프라이트 마스크)의 좌표가 태그에 일치하면(Ray)
+    // 스프라이트 마스크를 Instantiate 해준다
+
+    // 획순 순서에 따라?
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,8 @@ public class FollowMouseCursor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 target = Input.mousePosition;
+        target += new Vector3(0, 0, 1);
+        transform.position = Camera.main.ScreenToWorldPoint(target);
     }
 }
